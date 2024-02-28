ee8b99ce871de2ac4b12861e9b04b21f5bd14798
abijith <abijithumax100@gmail.com>
2024-02-28 10:51:00 -0500

    add js and copy bt

---
+++ script.js
@@ -0,0 +1,22 @@
+function copy1() {
+
+    navigator.clipboard.writeText
+     ("9745388290");
+    alert("number copied")
+    
+}
+
+ function copy2() {
+    navigator.clipboard.writeText
+      ("9562484096");
+    alert("number copied")
+    
+  }
+
+
+ function copy3() {
+    navigator.clipboard.writeText
+     ("8157038735");
+    alert("number copied")
+      
+  }
