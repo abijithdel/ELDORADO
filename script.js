a9fc6c04c111c599efcffd4bfb1c23082a231f1c
abijith <abijithumax100@gmail.com>
2024-03-02 17:40:20 -0500

    add mail system

--- script.js
+++ script.js
@@ -20,3 +20,31 @@ function copy1() {
     alert("number copied")
       
   }
+
+  
+
+
+  
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_waqrd6s";
+  const templateID = "template_5e4bl45";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
+
